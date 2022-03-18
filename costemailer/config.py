fa62db4cf64c13568546004b06ab98cae2efc75f
Chris Hambridge <chambrid@redhat.com>
2022-03-18 10:59:43 -0400

    Read recipients info from config file.
    * I want to move away from an environment variable to a mounted volume from a ConfigMap
    * Added a change to read a yaml file based on a default and specified by env location

--- costemailer/config.py
+++ costemailer/config.py
@@ -3,6 +3,8 @@ import json
 import logging
 import os
 
+import yaml
+
 
 LOG = logging.getLogger(__name__)
 
@@ -15,7 +17,8 @@ class Config:
     CLOUD_DOT_PASSWORD = os.getenv("CLOUD_DOT_PASSWORD")
     CLOUD_DOT_API_ROOT = os.getenv("CLOUD_DOT_API_ROOT", "https://cloud.redhat.com/api/")
 
-    COST_MGMT_RECIPIENTS = os.getenv("COST_MGMT_RECIPIENTS", "{}")
+    COST_MGMT_RECIPIENTS = {}
+    COST_MGMT_RECIPIENTS_FILE = os.getenv("COST_MGMT_RECIPIENTS_FILE", "/data/config.yaml")
     COST_MGMT_API_PREFIX = os.getenv("COST_MGMT_API_PREFIX", "cost-management/v1/")
 
     RBAC_API_PREFIX = os.getenv("RBAC_API_PREFIX", "rbac/v1/")
@@ -23,11 +26,20 @@ class Config:
     EMAIL_USER = os.getenv("EMAIL_USER")
     EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-    try:
-        COST_MGMT_RECIPIENTS = json.loads(COST_MGMT_RECIPIENTS)
-    except Exception as err:
-        LOG.error("Failed to parse COST_MGMT_RECIPIENTS", err)
-        exit(-1)
+    if os.path.exists(COST_MGMT_RECIPIENTS_FILE):
+        with open(COST_MGMT_RECIPIENTS_FILE, "r") as stream:
+            try:
+                COST_MGMT_RECIPIENTS = yaml.safe_load(stream)
+            except yaml.YAMLError as error:
+                LOG.error(error)
+
+    if COST_MGMT_RECIPIENTS == {}:
+        COST_MGMT_RECIPIENTS = os.getenv("COST_MGMT_RECIPIENTS", "{}")
+        try:
+            COST_MGMT_RECIPIENTS = json.loads(COST_MGMT_RECIPIENTS)
+        except Exception as err:
+            LOG.error("Failed to parse COST_MGMT_RECIPIENTS", err)
+            exit(-1)
 
     if CLOUD_DOT_USERNAME is None or CLOUD_DOT_PASSWORD is None:
         LOG.warning(
